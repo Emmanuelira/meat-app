5454004dcaddbbe915d7d2cb39077e2de2367864
DIR\emmanuel.lira <emmanuel.lira@accenture.com>
2021-05-21 12:17:06 -0300

    Tratamento de erro realizado para o GET de restaurants

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from "@angular/core";
 
 import { Restaurant } from "./restaurant/restaurant.model";
 import { MEAT_API } from "../app.api";
+import { ErrorHandler } from "app/app.error-handler";
 
 @Injectable()
 export class RestaurantsService {
@@ -13,7 +14,8 @@ export class RestaurantsService {
   
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
-      .map(response => response.json());
+      .map(response => response.json())
+      .catch(response => ErrorHandler.handleError(response));
   }
 
 }
