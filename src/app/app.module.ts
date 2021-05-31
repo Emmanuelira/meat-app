99e71e8010b34e79d165df938f31caa65ace26ea
DIR\emmanuel.lira <emmanuel.lira@accenture.com>
2021-05-31 15:07:14 -0300

    Criação das rotas e diretórios para os detalhes dos restaurantes na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-
-import { ROUTES } from './app.routes';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 
 import { RestaurantsService } from './restaurants/restaurants.service';
+
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { RestaurantsService } from './restaurants/restaurants.service';
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
