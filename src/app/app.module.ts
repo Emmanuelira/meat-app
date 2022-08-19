8dbb3ae6190cdf4fbe4fa8c2bd2f63f326bdffc8
Emmanuelira <emmanuel.nobrega.lira@gmail.com>
2022-08-18 21:36:10 -0300

    Configurando o shared module para compartilhar os providers sob demanda, usando a técnica de ModuleWithProviders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ROUTES } from './app.routes';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -35,8 +34,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     HttpModule,
-    SharedModule,
-    CoreModule,
+    SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES)
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
